832b4309e16421406df896ff1796e59dfb375e10
Sanchit Joshi <sanchityj@gmail.com>
2022-03-20 14:53:03 +0530

    added heroku url

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+
+const App = () => {
+
+
+  const baseURL = "https://sleepy-gorge-94512.herokuapp.com/coupon/discount"
+
+  const [amount, setAmount] = useState(3000)
+  const [coupon, setCoupon] = useState(null)
+
+  const [discount, setDiscount] = useState(0)
+  const [valid, setValid] = useState(false)
+
+  const handleChange = (e) => {
+    const val = e.target.value
+    setAmount(val)
+  }
+
+  const handleClick = (coupon) => {
+    setCoupon(coupon)
+  }
+
+  useEffect(() => {
+    axios.post(baseURL, {
+      totalAmount: amount,
+      couponCode: coupon
+    })
+      .then((response) => {
+        console.log(response.data, "output")
+        setValid(response.data.valid)
+
+        setDiscount(response.data.discount || 0)
+
+      }).catch((error) => {
+        console.log(error)
+      })
+
+  }, [amount, coupon])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h2>Enter Total Amount</h2>
+      <input
+        type="number"
+        onChange={(e) => handleChange(e)}
+        value={amount}
+      />
+      <br />
+      <br />
+      <h3>Apply Coupons Here..</h3>
+
+      <button onClick={(e) => handleClick("ZOM60")} type="button" className="btn btn-danger btn-space" href="#">ZOM60</button>
+
+      <button onClick={(e) => handleClick("SWI40")} type="button" className="btn btn-primary btn-space">SWI40</button>
+
+      <button onClick={(e) => handleClick("FRI50")} type="button" className="btn btn-warning btn-space">FRI50</button>
+
+      <button onClick={(e) => handleClick("AMZ40")} type="button" className="btn btn-dark btn-space">AMZ40</button>
+      <br />
+      <br />
+
+      <h3>Selected Coupon: {coupon}</h3>
+      <h3>Total discounted amount: {amount - discount}</h3>
+      
+      <h4>{valid ? "valid": "invalid"}</h4>
+
+
     </div>
   );
 }
 
-export default App;
+export default App
